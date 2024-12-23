6aedc18ac19659c8654171eef396bbff8aa3d0f1
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 05:13:22 +0800

    Menambahkan Menambahkan data Dao pada tabel Barang

--- app/src/main/java/com/example/ucp_2pam/Data/Dao/BarangDao.kt
+++ app/src/main/java/com/example/ucp_2pam/Data/Dao/BarangDao.kt
@@ -1,28 +1,23 @@
-package com.example.ucp_2pam.Data.Dao
+package com.example.ucp_2pam.data.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import com.example.ucp_2pam.Data.Entity.Barang
-    import kotlinx.coroutines.flow.Flow
-
+import com.example.ucp_2pam.Entity.Barang
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BarangDao {
     @Insert
     suspend fun insertBarang(barang: Barang)
-
-    @Query("SELECT * FROM barang")
-    suspend fun getAllBarang(): List<Barang>
-
     @Update
     suspend fun updateBarang(barang: Barang)
-
     @Delete
     suspend fun deleteBarang(barang: Barang)
-
-    @Query("SELECT * FROM barang WHERE id = :id")
-    suspend fun getDetailBarang(id: Int): Barang
+    @Query("SELECT * FROM barang ORDER BY namabarang ASC")
+    fun getAllBarang(): Flow<List<Barang>>
+    @Query("SELECT * FROM barang WHERE idbarang = :id")
+    fun getBarang(id: String): Flow<Barang>
 }
