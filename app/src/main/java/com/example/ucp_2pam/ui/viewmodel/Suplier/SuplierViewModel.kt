0c7ecb214c59390fed7d104c900316a4a1915d20
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 06:00:56 +0800

    buat data class SuplierUiState, FormSplErrorState, dan SuplierEvent untuk menangani input suplier

--- app/src/main/java/com/example/ucp_2pam/ui/viewmodel/Suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp_2pam/ui/viewmodel/Suplier/SuplierViewModel.kt
@@ -60,3 +60,34 @@ class SuplierViewModel(private val repositorySup: RepositorySpl) : ViewModel() {
         )
     }
 }
+
+data class SuplierUiState(
+    val suplierEvent: SuplierEvent = SuplierEvent(),
+    val isEntryValid: FormSplErrorState = FormSplErrorState(),
+    val snackbarMessage: String? = null
+)
+
+data class FormSplErrorState(
+    val idsuplier: String? = null,
+    val namasuplier: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null
+){
+    fun isValid(): Boolean{
+        return idsuplier == null && namasuplier == null && kontak == null && alamat == null
+    }
+}
+
+data class SuplierEvent(
+    val idsuplier: String = "",
+    val namasuplier: String = "",
+    val kontak: String = "",
+    val alamat: String = ""
+)
+
+fun SuplierEvent.toSuplierEntity(): Suplier = Suplier(
+    idsuplier = idsuplier,
+    namasuplier = namasuplier,
+    kontak = kontak,
+    alamat = alamat
+)
