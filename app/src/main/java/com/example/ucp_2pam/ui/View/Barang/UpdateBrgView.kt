e6cafb82e12a1686cabcb43c9142dfd81c191064
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 06:18:03 +0800

    Bikin Tampilan Update Barang dengan Snackbar dan Status Loading

--- app/src/main/java/com/example/ucp_2pam/ui/View/Barang/UpdateBrgView.kt
+++ app/src/main/java/com/example/ucp_2pam/ui/View/Barang/UpdateBrgView.kt
@@ -1,2 +1,49 @@
 package com.example.ucp_2pam.ui.View.Barang
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2pam.ui.CustomWidget.TopAppBar
+import com.example.ucp_2pam.ui.viewmodel.Barang.UpdateBrgViewModel
+import com.example.ucp_2pam.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UpdateBrgView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateBrgViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val BrgUiState = viewModel.UpdateBrgUiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(
+        BrgUiState.snackbarMessage
+    ) {
+        println("LaunchedEffect triggered")
+        BrgUiState.snackbarMessage?.let { message ->
+            println("Launhing coroutine for snackbar")
+            snackbarHostState.showSnackbar(
+                message = message,
+                duration = SnackbarDuration.Long,
+            )
+            viewModel.resetSnackbarMessage()
+        }
+    }
+
