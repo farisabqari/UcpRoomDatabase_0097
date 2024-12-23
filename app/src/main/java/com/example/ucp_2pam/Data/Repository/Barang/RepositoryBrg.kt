31c4f66a844947079f3c3d855d351a827e0a335a
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 05:27:03 +0800

    Membuat RepositoryBrg untuk mengelola data Barang

--- app/src/main/java/com/example/ucp_2pam/Data/Repository/Barang/RepositoryBrg.kt
+++ app/src/main/java/com/example/ucp_2pam/Data/Repository/Barang/RepositoryBrg.kt
@@ -3,6 +3,7 @@ package com.example.ucp_2pam.Data.Repository.Barang
 import com.example.ucp_2pam.Entity.Barang
 import kotlinx.coroutines.flow.Flow
 
+
 interface RepositoryBrg {
     suspend fun insertBarang(barang: Barang)
     fun getAllBarang(): Flow<List<Barang>>
