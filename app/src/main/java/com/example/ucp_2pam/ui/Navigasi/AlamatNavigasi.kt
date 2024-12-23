4b4d912f72327763f366ee921bb9bbf3d2fe9a5f
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 06:38:41 +0800

    buat file AlamatNavigasi dan Tambahkan objek Destinasi untuk Navigasi dengan Route

--- app/src/main/java/com/example/ucp_2pam/ui/Navigasi/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp_2pam/ui/Navigasi/AlamatNavigasi.kt
@@ -1,2 +1,43 @@
 package com.example.ucp_2pam.ui.Navigasi
 
+interface AlamatNavigasi{
+    val route: String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiHomeSpl: AlamatNavigasi {
+    override val route = "homesuplier"
+}
+
+object DestinasiHomeBrg: AlamatNavigasi {
+    override val route = "homebarang"
+}
+
+object DestinasiInsertSpl: AlamatNavigasi {
+    override val route = "suplier"
+}
+
+object DestinasiInsertBrg: AlamatNavigasi {
+    override val route = "barang"
+}
+
+object DestinasiDetailSpl: AlamatNavigasi {
+    override val route = "detail"
+    const val idSuplier = "idSuplier"
+    val routeWithArg = "$route/{$idSuplier}"
+}
+
+object DestinasiDetailBrg: AlamatNavigasi {
+    override val route = "detail"
+    const val idBarang = "idBarang"
+    val routeWithArg = "$route/{$idBarang}"
+}
+
+object DestinasiUpdate: AlamatNavigasi {
+    override val route = "update"
+    const val idBarang = "idBarang"
+    val routeWithArg = "$route/{$idBarang}"
+}
