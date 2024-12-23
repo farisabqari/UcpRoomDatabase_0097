05f6dd03bc86be7a4514e54bf84675612b7c00cc
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 05:45:48 +0800

    - Menambahkan data class BrgUiState untuk menyimpan state UI, termasuk data barang, status validasi, dan pesan snackbar - Menambahkan data class FormBrgErrorState untuk menampung pesan error tiap field saat validasi input barang - Menambahkan data class BarangEvent untuk menyimpan data yang diinput oleh pengguna di form barang Menambahkan toBarangEntity untuk mengubah BarangEvent menjadi entitas Barang

--- app/src/main/java/com/example/ucp_2pam/ui/viewmodel/Barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp_2pam/ui/viewmodel/Barang/BarangViewModel.kt
@@ -62,3 +62,41 @@ class BarangViewModel (private val repositoryBrg: RepositoryBrg) : ViewModel() {
     }
 }
 
+
+data class BrgUiState(
+    val barangEvent: BarangEvent = BarangEvent(),
+    val isEntryValid: FormBrgErrorState = FormBrgErrorState(),
+    val snackbarMessage: String? = null
+)
+
+data class FormBrgErrorState(
+    val idbarang: String? = null,
+    val namabarang: String? = null,
+    val deskripsi: String? = null,
+    val harga: String? = null,
+    val stok: String? = null,
+    val namasuplier: String? = null
+){
+    fun isValid(): Boolean {
+        return idbarang == null && namabarang == null && deskripsi == null &&
+                harga == null && stok == null && namasuplier == null
+    }
+}
+
+data class BarangEvent(
+    val idbarang: String = "",
+    val namabarang: String = "",
+    val deskripsi: String = "",
+    val harga: Int = 0,
+    val stok: Int = 0,
+    val namasuplier: String = ""
+)
+
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    idbarang = idbarang,
+    namabarang = namabarang,
+    deskripsi = deskripsi,
+    harga = harga,
+    stok = stok,
+    namasuplier = namasuplier
+)
