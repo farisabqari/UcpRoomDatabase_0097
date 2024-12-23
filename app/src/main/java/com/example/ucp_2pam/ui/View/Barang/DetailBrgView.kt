2e8ca8b5a17f7ab9137a1ce5eda4f495e134e69d
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 06:19:56 +0800

    Tambah Halaman DetailBarangView dengan Fitur Edit dan Hapus

--- app/src/main/java/com/example/ucp_2pam/ui/View/Barang/DetailBrgView.kt
+++ app/src/main/java/com/example/ucp_2pam/ui/View/Barang/DetailBrgView.kt
@@ -1,2 +1,64 @@
 package com.example.ucp_2pam.ui.View.Barang
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2pam.Entity.Barang
+import com.example.ucp_2pam.ui.CustomWidget.TopAppBar
+import com.example.ucp_2pam.ui.viewmodel.Barang.DetailBrgUiState
+import com.example.ucp_2pam.ui.viewmodel.Barang.DetailBrgViewModel
+import com.example.ucp_2pam.ui.viewmodel.Barang.toBarangEntity
+import com.example.ucp_2pam.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun DetailBrgView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailBrgViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Detail Barang",
+                showBackButton = true,
+                onBack = onBack,
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailBrgUiState.value.detailBrgUiEvent.idbarang)
+                },
+                shape = MaterialTheme.shapes.medium,
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+            }
+        }
+    ) { innerPadding ->
+        val detailBrgUiState by viewModel.detailBrgUiState.collectAsState()
+
+        BodyDetailBrg(
+            modifier = Modifier.padding(innerPadding),
+            detailBrgUiState = detailBrgUiState,
+            onDeleteClick = {
+                viewModel.deleteBrg()
+                onDeleteClick()
+            }
+        )
+    }
+}
+
