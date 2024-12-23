7f6fb2a40bfaca51beca116bd64394386c9c4caf
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 05:27:30 +0800

    Membuat RepositorySpl untuk mengelola data Suplier

--- app/src/main/java/com/example/ucp_2pam/Data/Repository/Suplier/RepositorySpl.kt
+++ app/src/main/java/com/example/ucp_2pam/Data/Repository/Suplier/RepositorySpl.kt
@@ -3,6 +3,7 @@ package com.example.ucp_2pam.Data.Repository.Suplier
 import com.example.ucp_2pam.Entity.Suplier
 import kotlinx.coroutines.flow.Flow
 
+
 interface RepositorySpl {
     suspend fun insertSuplier(suplier: Suplier)
     fun getAllSuplier(): Flow<List<Suplier>>
