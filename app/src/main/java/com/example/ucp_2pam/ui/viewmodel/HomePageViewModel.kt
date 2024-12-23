2ef05171876d542cbf85cbbfbd18ed7d02e4d80a
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 06:06:50 +0800

    Menambahkan ViewModel untuk mengelola data Barang dan Suplier secara bersamaan, menggunakan stateflow untuk mengelola state barang dan suplier

---
+++ app/src/main/java/com/example/ucp_2pam/ui/viewmodel/HomePageViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.ucp_2pam.ui.viewmodel
+
+import com.example.ucp_2pam.Data.Repository.Suplier.RepositorySpl
+import com.example.ucp_2pam.Entity.Barang
+import com.example.ucp_2pam.Entity.Suplier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp_2pam.Data.Repository.Barang.RepositoryBrg
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomePageViewModel(
+    private val repositoryBrg: RepositoryBrg,
+    private val repositorySup: RepositorySpl
+): ViewModel(){
+
+    val homeBrgUiState: StateFlow<HomeBrgUiState> = repositoryBrg.getAllBarang()
+        .filterNotNull()
+        .map{
+            HomeBrgUiState(
+                barangList = it.toList(),
+                isLoading = false,
+            )
+        }
+        .catch {throwable ->
+            emit(
+                HomeBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = throwable.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeBrgUiState(
+                isLoading = true
+            )
+        )
+    val homeSplUiState: StateFlow<HomeSplUiState> = repositorySup.getAllSuplier()
+        .filterNotNull()
+        .map{
+            HomeSplUiState(
+                listSuplier = it.toList(),
+                isLoading = false,
+            )
+        }
+        .catch { throwable ->
+            emit(
+                HomeSplUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = throwable.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeSplUiState(
+                isLoading = true
+            )
+        )
+}
+
