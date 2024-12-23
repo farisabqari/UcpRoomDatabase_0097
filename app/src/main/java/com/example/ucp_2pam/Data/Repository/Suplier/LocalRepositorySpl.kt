745458d6baa5292e2132e8ed519af86f710980e4
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 05:22:13 +0800

    Membuat LocalRepositoryBrg untuk mengelola data suplier

---
+++ app/src/main/java/com/example/ucp_2pam/Data/Repository/Suplier/LocalRepositorySpl.kt
@@ -0,0 +1,18 @@
+package com.example.ucp_2pam.Data.Repository.Suplier
+
+import com.example.ucp_2pam.Entity.Suplier
+import com.example.ucp_2pam.data.dao.SuplierDao
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySpl(
+    private val suplierDao: SuplierDao
+) : RepositorySpl {
+    override suspend fun insertSuplier(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+    override fun getAllSuplier(): Flow<List<Suplier>> =
+        suplierDao.getAllSuplier()
+
+    override fun getSuplier(id: Int): Flow<Suplier> =
+        suplierDao.getSuplier(id)
+}
