c299890a95330ec3be58879325f3e1e351939d17
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 05:13:53 +0800

    Menambahkan Menambahkan data Dao pada tabel Suplier

--- app/src/main/java/com/example/ucp_2pam/Data/Dao/SuplierDao.kt
+++ app/src/main/java/com/example/ucp_2pam/Data/Dao/SuplierDao.kt
@@ -1,16 +1,17 @@
-package com.example.ucp_2pam.Data.Dao
+package com.example.ucp_2pam.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import com.example.ucp_2pam.Data.Entity.Suplier
+import com.example.ucp_2pam.Entity.Suplier
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SuplierDao {
     @Insert
     suspend fun insertSuplier(suplier: Suplier)
-
-    @Query("SELECT * FROM suplier")
-    suspend fun getAllSuplier(): List<Suplier>
+    @Query("SELECT * FROM suplier ORDER BY namasuplier ASC")
+    fun getAllSuplier(): Flow<List<Suplier>>
+    @Query("SELECT * FROM suplier WHERE idsuplier = :id")
+    fun getSuplier(id: Int): Flow<Suplier>
 }
