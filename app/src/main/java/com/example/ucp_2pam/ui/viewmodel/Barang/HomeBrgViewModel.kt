5ddcb6c877d34f532c37ba04f4412d399b68cc8b
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 05:50:00 +0800

    menambahkan HomeBrgViewModel untuk pengelolaan data barang di halaman utama

---
+++ app/src/main/java/com/example/ucp_2pam/ui/viewmodel/Barang/HomeBrgViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.ucp_2pam.ui.viewmodel.Barang
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp_2pam.Data.Repository.Barang.RepositoryBrg
+import com.example.ucp_2pam.Entity.Barang
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeBrgViewModel(
+    private val repositoryBrg: RepositoryBrg
+) : ViewModel() {
+    val HomeBrgUiState: StateFlow<HomeBrgUiState> = repositoryBrg.getAllBarang()
+        .filterNotNull()
+        .map {
+            HomeBrgUiState(
+                listBrg = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeBrgUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeBrgUiState()
+        )
+
+}
+
