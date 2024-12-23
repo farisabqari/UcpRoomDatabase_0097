1aca01a934bfa6beabe46f8790fbcdfd97c080c9
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-24 06:26:08 +0800

    Tambah Tampilan Daftar Supplier dengan HomeSplView

---
+++ app/src/main/java/com/example/ucp_2pam/ui/View/Suplier/HomeSplView.kt
@@ -0,0 +1,82 @@
+package com.example.ucp_2pam.ui.View.Suplier
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2pam.Entity.Suplier
+import com.example.ucp_2pam.ui.CustomWidget.TopAppBar
+import com.example.ucp_2pam.ui.viewmodel.PenyediaViewModel
+import com.example.ucp_2pam.ui.viewmodel.Suplier.HomeSplUiState
+import com.example.ucp_2pam.ui.viewmodel.Suplier.HomeSplViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeSplView(
+    viewModel: HomeSplViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddSpl: () -> Unit = { },
+    onBack: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                TopAppBar(
+                    onBack = onBack,
+                    showBackButton = true,
+                    judul = "Daftar Supplier",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.HomeSplUiState.collectAsState()
+
+        BodyHomeSplView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
